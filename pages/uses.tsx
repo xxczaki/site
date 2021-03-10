bbb9cdebd9496f40c7daeaaa06c143327d388e66
Antoni Kepinski <xxczaki@pm.me>
2021-03-10 09:49:11 +0100

    update

--- pages/uses.tsx
+++ pages/uses.tsx
@@ -12,7 +12,7 @@ const Index: NextPage<unknown> = () => {
 			<h2>Desk</h2>
 			<p>Since November 2020 I&apos;m using a <b>MacBook Pro 13&quot;</b> with the following specs:</p>
 			<ul>
-				<li>Intel Core I5 2,0-3,8 GHz (10th generation)</li>
+				<li>Intel Core I5 2,0-3,8 GHz (10th gen)</li>
 				<li>16GB RAM (DDR4, 3733 MHz)</li>
 				<li>512GB SSD PCIe</li>
 				<li>4x Thunderbolt 3 (USB-C)</li>
@@ -21,7 +21,7 @@ const Index: NextPage<unknown> = () => {
 			<p>Before that I was using a custom PC with Linux Mint for about 3 years.</p>
 			<h2>Accessories</h2>
 			<p>I&apos;m using a USB hub (<b>Green Cell HUB GC Connect60 8in1</b>) in order to connect stuff like my 24&quot; <b>Dell U2410 monitor</b> (rather old now, but it works just fine) or the <b>Logitech MX Master 3</b> mouse to my laptop. My favorite MacBook stand is the <b>Twelve South Curve</b>. <b>Logitech K350</b> is my wireless, ergonomic keyboard.</p>
-			<p>I use <b>Bose QuietComfort 25 headphones</b> for anything sound-related.</p>
+			<p>I use <b>Bang &amp; Olufsen H9 (3rd gen) headphones</b> for anything sound-related.</p>
 			<h2>Other gear</h2>
 			<p>I&apos;m using <b>iPhone 8 (64GB)</b> as my phone. YubiKey 5C NFC is my security key. I keep smaller files on <b>SanDisk 64GB Ultra Dual Drive Luxe USB</b> and large backups on my <b>Samsung Portable SSD T7 (500GB)</b>.</p>
 			<h2>Software</h2>
@@ -31,7 +31,7 @@ const Index: NextPage<unknown> = () => {
 			<h2>Configuration</h2>
 			<p>Settings and preferences for my browser, terminal, code editor and other apps can be found in my personal <ExtLink target="_blank" rel="noopener noreferrer" href="https://github.com/xxczaki/dotfiles">dotfiles repositiory</ExtLink>.</p>
 			<h2>Paid services</h2>
-			<p>Since 2018, I use <b>ProtonMail</b> for keeping my emails private. Their paid plan allows me to connect a custom domain. I used to pay a small amount for the previously mentioned <b>1Password</b>, but since February 2021 I use the local vault feature. To keep non-confidential data synchronized across my devices, I use <b>iCloud</b>. I mostly use <b>Namecheap</b> for registering domains and <b>Vercel</b> for hosting small websites. I also rent a VPS in Iceland via <b>1984 hosting</b>, which I highly recommend for privacy-related reasons.</p>
+			<p>Since 2018, I use <b>ProtonMail</b> for keeping my emails private. Their paid plan allows me to connect a custom domain. I also pay a small amount for the previously mentioned <b>1Password</b>, even though since February 2021 I only use their local vault feature. To keep non-confidential data synchronized across my devices, I use <b>iCloud</b>. I mostly use <b>Namecheap</b> for registering domains and <b>Vercel</b> for hosting small websites. I also rent a VPS in Iceland via <b>1984 hosting</b>, which I highly recommend for privacy-related reasons. This website is running on it!</p>
 		</Main>
 	);
 };
