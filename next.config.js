031a446db90293a53866855a200a6e36f5c84b65
Antoni Kepinski <xxczaki@pm.me>
2021-04-01 01:58:21 +0200

    pull

--- next.config.js
+++ next.config.js
@@ -25,8 +25,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	experimental: {
 		optimizeFonts: true,
-		optimizeImages: true,
-		optimizeCss: true
+		optimizeImages: true
 	},
     future: {
         webpack5: true,
