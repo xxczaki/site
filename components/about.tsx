369c1181d3e4f4272252450da882c0cae822dd0e
Antoni Kepinski <xxczaki@pm.me>
2021-04-21 11:33:30 +0200

    Fix blur effect

--- components/about.tsx
+++ components/about.tsx
@@ -8,7 +8,7 @@ const About = (): JSX.Element => {
 		<section className="grid gap-12">
 			<div className="flex justify-between h-full">
 				<div className="flex flex-col max-w-xl w-full justify-evenly">
-					<h1 className="text-4xl font-bold pb-8 md:pb-0">Hi, my name is Antoni 👋</h1>
+					<h1 className="text-4xl font-bold pb-8 md:pb-0">Hi, I&apos;m Antoni 👋</h1>
 					<p>
 						Thanks for visiting my site! Below you can find more information about me and my projects.
 						If you need anything else please just drop me a <ExtLink href="https://twitter.com/dokwadratu"><b>Tweet</b></ExtLink> or <ExtLink href="mailto:antoni@kepinski.ch"><b>email</b></ExtLink> &lt;3
@@ -23,7 +23,7 @@ const About = (): JSX.Element => {
 						width={260}
 						height={260}
 						loading="eager"
-						className="absolute top-0 bottom-0 left-0 right-0 rounded-md object-cover object-center filter blur-md transform scale-125"
+						className="absolute top-0 bottom-0 left-0 right-0 rounded-md object-cover object-center filter blur-sm"
 					/>
 					<Image
 						className="select-none transition-all hover:filter hue-rotate-90 pointer-events-none rounded-md"
@@ -37,7 +37,7 @@ const About = (): JSX.Element => {
 			</div>
 			<h2 className="text-2xl font-bold">About</h2>
 			<p>
-				Hey, I’m Antoni. I’m a full stack developer and open-source enthusiast.
+				I’m a full stack developer and open-source enthusiast.
 				Since late 2019 I&apos;m maintaining <ExtLink href="https://github.com/node-fetch/node-fetch">node-fetch</ExtLink>, a lightweight library, which brings Fetch API to Node.js.
 				I also maintain a collection of my own open source projects.
 				I&apos;m currently in the 2nd year of high school (grade 11), focusing mainly on math, physics and computer science.
