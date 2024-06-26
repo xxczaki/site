5192fa42bdddf4abe8bc4f9bc28436470e56f710
Antoni Kępiński <antoni@kepinski.ch>
2024-06-26 02:23:10 +0200

    improve seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
+import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
+import robotsTxt from 'astro-robots-txt';
 import smartypants from "remark-smartypants";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  site: 'https://www.xxczaki.com',
+  integrations: [tailwind(), mdx(), sitemap(), robotsTxt()],
   markdown: {
     remarkPlugins: [smartypants]
   },
   output: "hybrid",
-  adapter: cloudflare({imageService: 'compile'})
+  adapter: cloudflare({
+    imageService: 'compile'
+  })
 });
